dc09d728324ba4bd51e24e6661cfbdda8748b9ce
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-03-15 21:30:54 -0600

    added switch statement to index.js and made placeholders for all of my functions.

--- index.js
+++ index.js
@@ -1,11 +1,121 @@
-const inquirer = require("inquirer");
+const { prompt } = require("inquirer");
 const cTable = require("console.table");
 const mysql = require("mysql2");
 
-// construct inquirer prompt - What should this look like / how many do I want?  Just 1 and switch statement? Or multiple prompts and no switch statement?
-
-// functions required to make the CLI app run.
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "business_db",
+});
 
 //contstruct db queries ? What all needs to be queried and what functions should they be placed in?
 
+//db.query for SELECT * employees (i.e. view all employees)
+
+//db.query for ADD employee ()
+
+//db.query for UPDATE employee role
+
+//db.query for View all roles
+
+//db.query for ADD role
+
+//db.query for View all Departments
+
+//db.query for ADD department
+
 // final function 'function init()' most likely, to restart the prompt when the user selects "Restart(?)" and exits out of the CLI app when the user selects "Quit".
+// const initQuestions = [{
+//     type: "list",
+//     name: "userchoice",
+//     message: "What would you like to do?",
+//     choices: Object.prompt.
+// }];
+
+// construct inquirer prompt - What should this look like / how many do I want?  Just 1 and switch statement? Or multiple prompts and no switch statement?
+const promptUser = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "userchoice",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
+        ],
+      },
+    ])
+    .then((data) => {
+      console.log(data);
+      switch (data.userchoice) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+
+        case "View All Roles":
+          viewAllRoles();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "View All Departments":
+          viewAllDepartments();
+          break;
+
+        case "Add Department":
+          addDepartment();
+          break;
+
+        case "Quit":
+          console.log("G'bye");
+          process.exit();
+      }
+    });
+};
+
+const viewAllEmployees = () => {
+  promptUser();
+};
+
+const addEmployee = () => {
+  promptUser();
+};
+
+const updateEmployeeRole = () => {
+  promptUser();
+};
+
+const viewAllRoles = () => {
+  promptUser();
+};
+
+const addRole = () => {
+  promptUser();
+};
+
+const viewAllDepartments = () => {
+  promptUser();
+};
+
+const addDepartment = () => {
+  promptUser();
+};
+
+// continue to make consts for each case in the switch and call your inquirer prompt inside each
